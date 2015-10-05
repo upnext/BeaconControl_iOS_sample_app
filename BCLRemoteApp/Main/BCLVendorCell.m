a0013ec012856d6bbd4de500e9025010e1870a62
Adrian Chojnacki <adrian.chojnacki@up-next.com>
2015-10-05 22:45:17 +0200

    vendor choice view controller

---
+++ BCLRemoteApp/Main/BCLVendorCell.m
@@ -0,0 +1,35 @@
+//
+//  BCLVendorCell.m
+//  BCLRemoteApp
+//
+// Copyright (c) 2015, Upnext Technologies Sp. z o.o.
+// All rights reserved.
+//
+// This source code is licensed under the BSD 3-Clause License found in the
+// LICENSE.txt file in the root directory of this source tree.
+//
+
+#import "BCLVendorCell.h"
+
+@interface BCLVendorCell()
+
+@property (weak, nonatomic) IBOutlet UILabel *checkmark;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@end
+
+@implementation BCLVendorCell
+
+- (void)setVendorName:(NSString *)vendorName selected:(BOOL)selected
+{
+    self.checkmark.hidden = !selected;
+    self.nameLabel.text = vendorName;
+}
+
+- (void)setActive:(BOOL)active
+{
+    _active = active;
+    self.nameLabel.textColor = active ? [UIColor blackColor] : [UIColor lightGrayColor];
+}
+
+@end
---
+++ BCLRemoteApp/Main/BCLVendorChoiceViewController.m
@@ -0,0 +1,99 @@
+//
+//  BCLVendorChoiceViewController.m
+//  BCLRemoteApp
+//
+// Copyright (c) 2015, Upnext Technologies Sp. z o.o.
+// All rights reserved.
+//
+// This source code is licensed under the BSD 3-Clause License found in the
+// LICENSE.txt file in the root directory of this source tree.
+//
+
+#import "BCLVendorChoiceViewController.h"
+#import "BCLVendorCell.h"
+
+@interface BCLVendorChoiceViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic) NSUInteger selectedVendorIndex;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+static NSArray *_vendors;
+static const NSUInteger BCLKontaktVendorIndex = 5;
+
+@implementation BCLVendorChoiceViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableView.tableFooterView = [UIView new];
+}
+
+- (NSArray *)vendors
+{
+    if (!_vendors) {
+        _vendors = @[
+                @"BlueCats",
+                @"BlueSense",
+                @"Estimote",
+                @"Glimworm",
+                @"Gimbal",
+                @"Kontakt (Use Admin Panel)",
+                @"Sensorberg",
+                @"Sonic Notify",
+                @"Other"
+        ];
+    }
+
+    return _vendors;
+}
+
+- (void)setSelectedVendor:(NSString *)selectedVendor
+{
+    _selectedVendor = selectedVendor;
+    NSUInteger vendorIndex = [self.vendors indexOfObject:selectedVendor];
+    if (vendorIndex == NSNotFound) {
+        vendorIndex = self.vendors.count - 1;
+    }
+
+    self.selectedVendorIndex = vendorIndex;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.vendors.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *vendorName = self.vendors[(NSUInteger) indexPath.row];
+    BCLVendorCell *vendorCell = (BCLVendorCell *) [tableView dequeueReusableCellWithIdentifier:@"vendorCell"];
+    [vendorCell setVendorName:vendorName selected:(self.selectedVendorIndex == indexPath.row)];
+    vendorCell.active = indexPath.row != BCLKontaktVendorIndex;
+    return vendorCell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row != BCLKontaktVendorIndex) {
+        self.selectedVendor = self.vendors[indexPath.row];
+        [tableView reloadData];
+    }
+}
+
+- (IBAction)doneButtonPressed:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(vendorChoiceViewController:didChooseVendor:)]) {
+        [self.delegate vendorChoiceViewController:self didChooseVendor:self.selectedVendor];
+    }
+}
+
+- (IBAction)cancelButtonPressed:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(vendorChoiceViewControllerDidCancel:)]) {
+        [self.delegate vendorChoiceViewControllerDidCancel:self];
+    }
+}
+
+@end

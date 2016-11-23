2516b30d32d545cd746cbb2c439e9ffceadba575
Minh Le Ngoc <minh.le.ngoc@upnext.io>
2016-11-23 11:51:17 +0100

    Updated Mapbox to 3.3

---
+++ BCLRemoteApp/Main/BCLAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  BCLAnnotation.m
+//  BCLRemoteApp
+//
+//  Created by minh on 17/11/16.
+//  Copyright © 2016 UpNext. All rights reserved.
+//
+
+#import "BCLAnnotation.h"
+
+@implementation BCLAnnotation
+
+@end
